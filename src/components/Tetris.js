5dc6892c4061bb657180e809f9c48fd2e8660bcb
Michael Su <xmichael.su@gmail.com>
2019-09-23 18:01:38 -0700

    styled tetris grids

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import Stage from './Stage';
+import GameArea from './GameArea';
 import Display from './Display';
 import StartButton from './StartButton';
 
 import { createGameArea } from '../Helpers';
+import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
 
 const Tetris = () => {
-  {
-    console.log(createGameArea());
-  }
   return (
-    <div>
-      <Stage stage={createGameArea()} />
+    <StyledTetrisWrapper>
+      <StyledTetris>
+        <GameArea area={createGameArea()} />
 
-      <scoreTracker>
-        <div>
-          <Display text="Score"></Display>
-          <Display text="Rows"></Display>
-          <Display text="Level"></Display>
-        </div>
-        <StartButton />
-      </scoreTracker>
-    </div>
+        <scoreTracker>
+          <div>
+            <Display text="Score"></Display>
+            <Display text="Rows"></Display>
+            <Display text="Level"></Display>
+          </div>
+          <StartButton />
+        </scoreTracker>
+      </StyledTetris>
+    </StyledTetrisWrapper>
   );
 };
 
