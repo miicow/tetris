c6eab3f1898ce696fbd540c2d94c849af7bac0e3
Michael Su <xmichael.su@gmail.com>
2019-09-23 18:01:18 -0700

    styled cell components

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import { StyledCell } from './styles/StyledCell';
+import { TETROMINOS } from '../tetrominos';
 
-const Cell = ({ type }) => <div className="Cell">cell</div>;
-
+const Cell = ({ type }) => (
+  //color property access the shape and color property
+  <StyledCell type={'L'} color={TETROMINOS['L'].color} />
+);
 export default Cell;
