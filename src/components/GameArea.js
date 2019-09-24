673bcde3ef4557e8e35a9a247b3a1de160fa102d
Michael Su <xmichael.su@gmail.com>
2019-09-23 18:01:51 -0700

    styled game play area

--- src/components/GameArea.js
+++ src/components/GameArea.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import Cell from './Cell';
+import { StyledArea } from './styles/StyledArea';
 
-const Stage = ({ stage }) => (
-  <div>
+const GameArea = ({ area }) => (
+  <StyledArea width={area[0].length} height={area.length}>
     {/* 
     map through the array created by the helper func
     then map through the arrays in the row
     within the row, create cell component with x and first element defined in the helper function
     */}
-    {stage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]} />))}
-  </div>
+    {area.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]} />))}
+  </StyledArea>
 );
 
-export default Stage;
+export default GameArea;
